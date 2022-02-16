6a97423612aca269d5f239f6f689a18fed2b960b
cbesancenez2 <94849129+cbesancenez2@users.noreply.github.com>
2022-02-16 09:32:31 -0600

    updates to inheritence between controller parent and child classes

--- controller.py
+++ controller.py
@@ -19,3 +19,14 @@ class Controller:
                                         brightness=1,
                                         pixel_order=neopixel.RGB)
         self.pixels.fill((0,0,0))
+
+    def process_event(self, event):
+        message, deltatime = event
+        state = message[0]
+        print(message, deltatime)
+        if state == Controller.DOWN:
+            self.pixels[self.next_light % self.num_lights] = self.color_on
+            self.next_light+=1
+        else:
+            self.pixels[self.prev_light % self.num_lights] = self.color_off
+            self.prev_light+=1
--- rainbow_controller.py
+++ rainbow_controller.py
@@ -1,28 +1,13 @@
-# libraries for controlling lights
-import board
-import neopixel
-
 class RainbowController(Controller):
-    #DOWN = 144
 
     def __init__(self, num_lights):
        super().__init__(self,num_lights)
-        # state of the lights
-        #self.num_lights = num_lights
         self.color_on = (255,255,255)
         self.color_off = (0,0,0)
-       # self.next_light = 0
-       # self.prev_light = 0
-
-        # initialize the lights array, turn all lights off
-        #self.pixels = neopixel.NeoPixel(board.D18,
-        #                                self.num_lights,
-        #                                brightness=1,
-        #                                pixel_order=neopixel.RGB)
-        #self.pixels.fill((0,0,0))
 
 
     def process_event(self, event):
+        super().process_event(self,event)
         self.color_off = self.color_on
         if self.next_light % (self.num_lights * 2) == 0:
             self.next_light = 0
@@ -31,20 +16,9 @@ class RainbowController(Controller):
         self.color_on = wheel(self.next_light * 256 // self.num_lights )
 
         # Only turn on one light for a chord
-        message, deltatime = event
         if deltatime < 0.02:
             print('chord')
             return
-
-        print(message, deltatime)
-        state = message[0]
-
-        if state == RainbowController.DOWN:
-            self.pixels[self.next_light % self.num_lights] = self.color_on
-            self.next_light+=1
-        else:
-            self.pixels[self.prev_light % self.num_lights] = self.color_off
-            self.prev_light+=1
 
 ##Funtion that makes each light a different rainbow color
 def wheel(pos):
